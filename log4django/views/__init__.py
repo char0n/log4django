c6c0fdd69b79c3b00a58a25afec3962e9d9fb625
Juda Kaleta <juda.kaleta@gmail.com>
2013-10-24 13:34:13 +0200

    Filtering by search query

--- log4django/views/__init__.py
+++ log4django/views/__init__.py
@@ -1,3 +1,5 @@
+from django.db.models import Q
+
 from ..models import LogRecord
 
 
@@ -5,6 +7,16 @@ def _filter_records(request):
     getvars = request.GET
     logrecord_qs = LogRecord.objects.all().select_related('app')
     # Filtering by get params.
+    if getvars.get('q'):
+        q = getvars.get('q')
+        logrecord_qs = logrecord_qs.filter(
+            Q(app__name__icontains=q)
+            | Q(message__icontains=q)
+            | Q(fileName__icontains=q)
+            | Q(loggerName__icontains=q)
+            | Q(exception_message__icontains=q)
+            | Q(_extra__icontains=q)
+        )
     if getvars.get('app'):
         logrecord_qs = logrecord_qs.filter(app_id=getvars.get('app'))
     if getvars.get('logger'):
