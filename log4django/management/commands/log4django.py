c975d676efd4d03d1346a3c29da250eb4832c2a6
Vladimir Gorej <gorej@codescale.net>
2013-12-11 14:26:15 +0100

    Version 0.6.0.
    - fixes slow computing of logger names; only logger names from current page are now computed
    - fixes transaction management in background worker

--- log4django/management/commands/log4django.py
+++ log4django/management/commands/log4django.py
@@ -15,12 +15,11 @@ class Command(GearmanWorkerBaseCommand):
     def task_name(self):
         return GERMAN_TASK_NAME
 
+    @transaction.commit_manually(using=settings.DATABASE_ALIAS_LOGGING)
     def do_job(self, job_data):
         try:
             payload = json.loads(job_data)
             return persist_record(payload)
         except Exception, ex:
             print ex
-        finally:
-            transaction.commit(using=settings.DATABASE_ALIAS_LOGGING)
-        return True
+        return False
--- log4django/views/logrecord/__init__.py
+++ log4django/views/logrecord/__init__.py
@@ -3,7 +3,6 @@ from django.db.models import Q
 from django.shortcuts import get_object_or_404
 from django.utils.decorators import method_decorator
 from django.views.generic import TemplateView
-from django.core.cache import cache
 
 from ...models import LogRecord, App
 from ...settings import PAGE_SIZE
@@ -30,29 +29,12 @@ class LogRecordList(TemplateView):
             records = paginator.page(paginator.num_pages)
         # Getting filtering values.
         apps = App.objects.all()
-        try:
-            loggers = self.get_loggers_names()
-        except NotImplementedError:
-            loggers = LogRecord.objects.order_by('loggerName').values('loggerName')
-            loggers = set([r['loggerName'] for r in loggers])
-            loggers = [{'loggerName': ln} for ln in loggers]
+        loggers = set([r.loggerName for r in records])
         levels = LogRecord.LEVEL
         return self.render_to_response(dict(
             records=records, apps=apps, loggers=loggers, levels=levels,
             filter_levels=[int(l) for l in request.GET.getlist('level')]
         ))
-
-    @staticmethod
-    def get_loggers_names():
-        cache_key = 'log4django_loggers_names'
-        loggers = cache.get(cache_key)
-        if loggers is None:
-            loggers = list(
-                LogRecord.objects.distinct('loggerName').order_by('loggerName').values('loggerName')
-            )
-            cache.set(cache_key, loggers)
-        return loggers
-
 
 
 class LogRecordDetail(TemplateView):
