9f2fd46f924419d8789525b893bfadcc000c2278
Juda Kaleta <juda.kaleta@gmail.com>
2014-02-19 15:03:25 +0100

    Remove transactions from log4django command

--- log4django/management/commands/log4django.py
+++ log4django/management/commands/log4django.py
@@ -1,8 +1,5 @@
 import json
 
-from django.db import transaction
-from django.conf import settings
-
 from django_gearman_commands import GearmanWorkerBaseCommand
 
 from ...settings import GERMAN_TASK_NAME, COMMAND_EXCEPTION_CALLBACK
@@ -14,30 +11,15 @@ exception_callback = import_string(COMMAND_EXCEPTION_CALLBACK)
 
 
 class Command(GearmanWorkerBaseCommand):
-    db = settings.DATABASE_ALIAS_LOGGING
-
     @property
     def task_name(self):
         return GERMAN_TASK_NAME
 
-    def exiting(self):
-        try:
-            if transaction.is_dirty(using=self.db):
-                try:
-                    transaction.commit(using=self.db)
-                except:
-                    transaction.rollback(using=self.db)
-        finally:
-            transaction.leave_transaction_management(using=self.db)
-
     def do_job(self, job_data):
-        transaction.enter_transaction_management(using=self.db)
         try:
             payload = json.loads(job_data)
             return persist_record(payload)
         except Exception as ex:
             exception_callback(ex, gearman_worker=self.gearman_worker)
-        finally:
-            self.exiting()
 
         return False
